7f599ef3d445404df3fc1193f211161d52b5ed53
angelDariaux <angeldariaux@gmail.com>
2022-08-07 13:40:03 -0600

    full outer joins

--- SQL_code/joins.sql
+++ SQL_code/joins.sql
@@ -6,15 +6,36 @@
 
 SELECT * FROM payment 
 INNER JOIN customer
-ON payment.customer_id = customer.customer_id
+ON payment.customer_id = customer.customer_id;
 
 
 SELECT payment_id, payment.customer_id, first_name 
 FROM payment
 INNER JOIN customer
-ON payment.customer_id = customer.customer_id
+ON payment.customer_id = customer.customer_id;
 
 
-        -- ! OUTER JOINS
+      -- ! OUTER JOINS
 
 
+SELECT * FROM customer
+FULL OUTER JOIN payment
+ON customer.customer_id = payment.customer_id;
+
+
+-- This show an empty table, meaning that there is not customer info who have never do a purchase in our store.
+SELECT * FROM customer
+FULL OUTER JOIN payment
+ON customer.customer_id = payment.customer_id
+WHERE customer.customer_id IS null
+OR payment.payment_id IS null;
+
+
+-- other way to check that there is not "extra" clients in our data base is the next one:
+
+SELECT COUNT(DISTINCT (customer_id)) FROM customer;
+
+SELECT COUNT(DISTINCT (customer_id)) FROM payment; 
+
+
+      -- ! LEFT OUTER JOINS
